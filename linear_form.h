64844247fd1304cbe94add6ba4cc3d9398c0b894
petrbel2 <petr.belolikov@yandex.ru>
2026-05-14 01:50:40 +0300

    comments

--- linear_form.h
+++ linear_form.h
@@ -10,16 +10,22 @@ typedef struct linear_form {
 } linear_form;
 //вычисление линейной формы, возвращает ссылку на результат
 void* calculate(linear_form base);
+
 //сложение линейных формрезультат перезаписывает первую форму)
 void addition(linear_form form1, linear_form form2);
+
 //вычитание линейных форм(результат перезаписывает первую форму)
 void subtraction(linear_form form1, linear_form form2);
+
 //умножение линейной формы на скаляррезультат перезаписывает форму)
 void multiplication(linear_form form1, float multiplier);
+
 //вывод линейной формы
 void print_form(linear_form form);
+
 //создание линейной формы(заполненной 1)
 linear_form create_linear_form(FieldInfo* (*of_field)(), int size);
+
 //заполнение линейной формы произвольными значениями(перезаписывает форму)
 void set_linear_form(linear_form base);
 
